d9990dbc7155843beebdd67b39ff4b6c56fe8ab9
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-10-11 23:20:04 -0300

    strapi

---
+++ back-end/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '02918353886c62ffd856db8089cf9e19'),
+    },
+  },
+});
---
+++ back-end/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'af9cdf22-38ed-4100-82f8-924fa5c202c5'
+};
